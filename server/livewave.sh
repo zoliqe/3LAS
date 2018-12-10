eebffa282ebd3984ed0ff08bec81e6095d2e7a9a
Michal Karas <zoliqe@gmail.com>
2018-12-10 21:48:35 +0100

    Improvements for remotig server
    Lowered bitrate to 80kbps; used tmux session to run in backgroud, used dnsoop alsa device as input

--- server/livewave.sh
+++ server/livewave.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-ffmpeg -y -f alsa -i hw:0 -rtbufsize 64 -probesize 64 \
--acodec pcm_s16le -ar 16000 -ac 1 -f wav -fflags +nobuffer \
+# run streaming service in background - to access process console use "tmux attach-session"
+tmux new-session -d -s remotig-audio "ffmpeg -y -f alsa -i plug:dsnoop -rtbufsize 64 -probesize 64 \
+-acodec pcm_s16le -ar 4000 -ac 1 -f wav -fflags +nobuffer \
 -packetsize 384 - \
-| nodejs stdinstreamer.js -port 9602 -type wav -chunksize 384
+| node stdinstreamer.js -port 9602 -type wav -chunksize 384"
